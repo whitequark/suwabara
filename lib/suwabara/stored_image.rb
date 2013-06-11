baf6d1440792959171ba17cf26066e40c0a0b765
Peter Zotov <whitequark@whitequark.org>
2013-06-11 15:17:33 +0400

    Store and extract width and height of a StoredImage.

--- lib/suwabara/stored_image.rb
+++ lib/suwabara/stored_image.rb
@@ -1,6 +1,32 @@
 module Suwabara
 
   class StoredImage < StoredFile
+
+    attr_reader :width, :height
+
+    def initialize_from_io(file, original_name)
+      image = MiniMagick::Image.read(file)
+
+      @width  = image[:width]
+      @height = image[:height]
+
+      super
+    end
+
+    def initialize_from_hash(hash)
+      @width  = hash["width"]
+      @height = hash["height"]
+
+      super
+    end
+
+    def to_hash
+      {
+        "width"  => @width,
+        "height" => @height,
+      }.merge(super)
+    end
+
     def url(transform = nil)
       transform = ImageTransform.parse(transform)
 
